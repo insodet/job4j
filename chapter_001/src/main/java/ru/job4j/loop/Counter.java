a8b764352883dad2cd1fff506637a1a2caa22c16
Roman Sukhorukov <ingeneerz@gmail.com>
2019-01-04 18:50:20 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне

---
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,29 @@
+package ru.job4j.loop;
+
+/**
+ * Класс для подсчета суммы четных чисел
+ */
+public class Counter {
+
+    /**
+     * Вычисляет сумму четных чисел в диапазоне
+     * @param start начальное число
+     * @param finish конечное число
+     * @return сумма
+     */
+    public int add(int start, int finish)
+    {
+        int result = 0;
+        for (int i = start; i <= finish; i++)
+        {
+            if (i % 2 != 0)
+            {
+                continue;
+            }
+
+            result = result + i;
+        }
+
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class CounterTest {
+
+    @Test
+    public void whenAdd1to10() {
+        Counter counter = new Counter();
+
+        int result = counter.add(1, 10);
+
+        assertThat(result, is(30));
+    }
+}
