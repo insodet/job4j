c62f1a60c75cfa204a8295e78f55d83ca6b5d895
Roman Sukhorukov <ingeneerz@gmail.com>
2019-01-04 14:18:18 +0300

    4.2. Максимум из двух чисел. Добавил javadoc

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -1,7 +1,16 @@
 package ru.job4j.max;
 
+/**
+ * This class is able to compare numbers. @see #max
+ */
 public class Max {
 
+    /**
+     * Return maximum of two integers.
+     * @param first - first number
+     * @param second - second number
+     * @return maximum number.
+     */
     public int max(int first, int second)
     {
         return (first > second) ? first : second;
